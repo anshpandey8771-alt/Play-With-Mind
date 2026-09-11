afd5b2bb43538840256898d8e767cc164466e007
Ansh <anshpandey8771@gmail.com>
2026-09-11 15:46:46 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/playwithmind/game/MainActivity.java
@@ -0,0 +1,28 @@
+package com.playwithmind.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
